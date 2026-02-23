edce114cb402bd4989f2351fee80afb88ee30d1d
Adair-Carlos-Fernandes <adaircnc@hotmail.com>
2026-02-23 19:41:39 -0300

    locadora

--- app/Models/carro.php
+++ app/Models/carro.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class locadora extends Model
+class carro extends Model
 {
     use HasFactory;
 }
--- database/migrations/2026_02_11_231638_create_carros_table.php
+++ database/migrations/2026_02_11_231638_create_carros_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('locadoras', function (Blueprint $table) {
+        Schema::create('carros', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('locadoras');
+        Schema::dropIfExists('carros');
     }
 };
